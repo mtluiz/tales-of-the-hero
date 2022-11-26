91dc842d618c5860f641215baf3fef613c263d94
mt <matheusluizn@gmail.com>
2022-11-25 21:08:49 -0300

    feat: create weapon action

--- src/weapon.py
+++ src/weapon.py
@@ -8,7 +8,17 @@ class Weapon(pygame.sprite.Sprite):
 
     #graphic 
     self.image = pygame.Surface((40, 40))
+    full_path = f'assets/graphics/weapons/{player.weapon}/{direction}.png'
+    self.image = pygame.image.load(full_path).convert_alpha()
 
     #placement
     if direction == 'right':
-      self.rect = self.image.get_rect(midleft = player.rect.center)
+      self.rect = self.image.get_rect(midleft = player.rect.midright + pygame.math.Vector2(0, 16))
+    elif direction == 'left':
+      self.rect = self.image.get_rect(midright = player.rect.midleft + pygame.math.Vector2(0, 16))
+    elif direction == 'down':
+      self.rect = self.image.get_rect(midtop = player.rect.midbottom + pygame.math.Vector2(-10, 0))
+    elif direction == 'up':
+      self.rect = self.image.get_rect(midbottom = player.rect.midtop + pygame.math.Vector2(-10, 0))
+    else: 
+      self.rect = self.image.get_rect(center = player.rect.center) 
