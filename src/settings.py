6d23249e04ce693aa212d5af8f2eb52a9fecf93b
mt <matheusluizn@gmail.com>
2022-11-25 21:09:22 -0300

    feat: add new ui variables

--- src/settings.py
+++ src/settings.py
@@ -11,6 +11,25 @@ weapon_data = {
   'sai' : {'cooldown' : 100, 'damage' : 15, 'graphic': "assets/graphics/weapons/sai/full.png"},
 }
 
+# general colors
+WATER_COLOR = '#71ddee'
+UI_BG_COLOR = '#222222'
+UI_BORDER_COLOR = '#111111'
+TEXT_COLOR = '#EEEEEE'
+ 
+# ui 
+BAR_HEIGHT = 20
+HEALTH_BAR_WIDTH = 200
+ENERGY_BAR_WIDTH = 140
+ITEM_BOX_SIZE = 80
+UI_FONT = 'assets/graphics/font/joystix.ttf'
+UI_FONT_SIZE = 18
+
+# ui colors
+HEALTH_COLOR = 'red'
+ENERGY_COLOR = 'blue'
+UI_BORDER_COLOR_ACTIVE = 'gold'
+
 WORLD_MAP = [
 ['x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x'],
 ['x',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','x'],
